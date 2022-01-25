228a8347a4ff92a626b697c602e32dae6b09b52a
launcher-dev <61925611+launcher-dev@users.noreply.github.com>
2022-01-24 19:35:32 -0600

    Update user_data_script.sh

--- user_data_script.sh
+++ user_data_script.sh
@@ -1,18 +1,9 @@
-#!/bin/bash -ex
-# output user data logs into a separate file for debugging
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-# download nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-# source nvm
-. /.nvm/nvm.sh
-# install node
-nvm install node
-#export NVM dir
-export NVM_DIR="/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
-#upgrade yum
+sudo yum install curl -y 
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.bashrc
+nvm install 16.9.0
 sudo yum upgrade
+sudo yum update -y
 #install git
 sudo yum install git -y
 cd /home/ec2-user
@@ -25,4 +16,4 @@ sudo chmod -R 755 .
 #install node module
 npm install
 # start the app
-node app.js > app.out.log 2> app.err.log < /dev/null &
+node app.js
